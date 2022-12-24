f482d307f37c27187c1f492993871000aa4091e8
Akash9796 <akashshukla0855@gmail.com>
2022-12-24 13:29:40 +0530

    2nd change to data base

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGODB_URL, {
+    .connect("mongodb+srv://AkashShukla01:shraddha@cluster0.nnct45g.mongodb.net/Ecommerce?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
     })
     .then((data) => {
