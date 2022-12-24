b9c2d7c676c48cdaafee69a7d9129664a63ae972
Akash9796 <akashshukla0855@gmail.com>
2022-12-24 13:40:57 +0530

    3rd change to db

--- config/database.js
+++ config/database.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect("mongodb+srv://AkashShukla01:shraddha@cluster0.nnct45g.mongodb.net/Ecommerce?retryWrites=true&w=majority", {
-      useUnifiedTopology: true,
-    })
+    .connect("mongodb+srv://AkashShukla01:shraddha@cluster0.nnct45g.mongodb.net/Ecommerce?retryWrites=true&w=majority")
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     })
